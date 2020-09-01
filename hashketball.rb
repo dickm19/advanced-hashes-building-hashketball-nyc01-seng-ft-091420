5169eaf5393b7c106c6310afc80e2b3bc2fa2353
Mary Dick <maryclairedick@gmail.com>
2020-09-01 19:34:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,7 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black","White"],
-      :players => [
-        {
+      :players => [{
           :player_name => "Alan Anderson",
           :number => 0,
           :shoe => 16,
@@ -59,8 +58,7 @@ def game_hash
           :steals => 4,
           :blocks => 11,
           :slam_dunks => 1
-        }
-        ]
+        }]
     }
     # :away => {
     #   :team_name => ""
