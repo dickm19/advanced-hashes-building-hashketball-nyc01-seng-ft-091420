8e626b60ba3e636a613d5d9b74167dd1c461c08a
Mary Dick <maryclairedick@gmail.com>
2020-09-01 19:28:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -62,18 +62,19 @@ def game_hash
         },
         ]
     },
-    :away => {
-      :team_name => ""
-      :colors => []
-      :players => 
-      [:player_name => "",
-      :number => "",
-      :shoe => "",
-      :points => "",
-      :rebounds => "",
-      :assists => "",
-      :steals => "",
-      :blocks => "",
-      :slam_dunks => ""]
-    }
+    # :away => {
+    #   :team_name => ""
+    #   :colors => []
+    #   :players => 
+    #   [:player_name => "",
+    #   :number => "",
+    #   :shoe => "",
+    #   :points => "",
+    #   :rebounds => "",
+    #   :assists => "",
+    #   :steals => "",
+    #   :blocks => "",
+    #   :slam_dunks => ""]
+    # }
   }
+  
