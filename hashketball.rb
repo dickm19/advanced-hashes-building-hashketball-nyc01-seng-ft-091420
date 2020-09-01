e9cdcb7c2ea18f929e348d90e4badcebaef11a94
Mary Dick <maryclairedick@gmail.com>
2020-09-01 19:22:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,36 @@
 # Write your code here!
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black, White"],
+      :players => [
+        {
+          :player_name => "",
+          :number => "",
+          :shoe => "",
+          :points => "",
+          :rebounds => "",
+          :assists => "",
+          :steals => "",
+          :blocks => "",
+          :slam_dunks => ""
+        },
+        
+        ]
+    },
+    :away => {
+      :team_name => ""
+      :colors => []
+      :players => 
+      [:player_name => "",
+      :number => "",
+      :shoe => "",
+      :points => "",
+      :rebounds => "",
+      :assists => "",
+      :steals => "",
+      :blocks => "",
+      :slam_dunks => ""]
+    }
+  }
